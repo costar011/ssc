6180548d409c21589798d49356bddb620dffbac6
costar011 <dnaer456@gmail.com>
2024-12-24 20:13:30 +0900

    wrapper

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,5 @@
 import styled from "@emotion/styled";
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 const Style = styled.div`
   display: flex;
@@ -11,15 +9,44 @@ const Style = styled.div`
 
   width: 100%;
   height: 100vh;
-  
+  gap: 1.5em;
   position: relative;
+
   z-index: 1;
+
+  @media (max-width: 768px) {
+    &:before {
+      width: 80%;
+      height: 80%;
+    }
+  }
 `;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: space-between;
+  justify-content: space-between;
+  align-items: space-between;
+
+  width: 100%;
+`;
+
+const LogoHeader = () => {
+    alert("Logo Clicked");
+};
+
+const LoginHeader = () => {
+    alert("Login Clicked");
+};
+
 const Main = () => {
   return (
-    <div>
-        <h1>Hello world</h1>
-    </div>
+    <Style>
+      <HeaderContainer>
+        <Button className="logo" onClick={LogoHeader}>Logo</Button>
+        <Button className="login" onClick={LoginHeader}>Login</Button>
+      </HeaderContainer>
+    </Style>
   );
 };
 
