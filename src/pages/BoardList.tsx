c98c849312b168654d8693352f393418dcdd0e5f
costar011 <dnaer456@gmail.com>
2024-12-29 12:50:46 +0900

    create button

--- src/pages/BoardList.tsx
+++ src/pages/BoardList.tsx
@@ -8,6 +8,9 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { TableVirtuoso, TableComponents } from "react-virtuoso";
 import Chance from "chance";
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import Stack from '@mui/material/Stack';
 
 interface Data {
   id: number;
@@ -25,52 +28,68 @@ interface ColumnData {
   width?: number;
 }
 
-const chance = new Chance(42);
+const Style = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  width: 100%;
+  position: relative;
+`;
+
+const WriteButton = styled(Button)`
+`;
+
+const chance = new Chance(42);
+// createData 함수는 주어진 id를 사용하여 랜덤 데이터를 생성
 function createData(id: number): Data {
   return {
     id,
-    firstName: chance.first(),
-    lastName: chance.last(),
-    age: chance.age(),
-    phone: chance.phone(),
-    state: chance.state({ full: true }),
+    firstName: chance.first(), // 랜덤한 이름을 생성
+    lastName: chance.last(), // 랜덤한 성을 생성
+    age: chance.age(), // 랜덤한 나이를 생성
+    phone: chance.phone(), // 랜덤한 전화번호를 생성
+    state: chance.state({ full: true }), // 랜덤한 주소를 생성
   };
 }
 
+// columns 배열은 테이블의 열에 대한 정보를 담고 있음
 const columns: ColumnData[] = [
   {
     width: 100,
-    label: "First Name",
-    dataKey: "firstName",
+    label: "First Name", // 열의 제목으로 "First Name"을 사용
+    dataKey: "firstName", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
   },
   {
     width: 100,
-    label: "Last Name",
-    dataKey: "lastName",
+    label: "Last Name", // 열의 제목으로 "Last Name"을 사용
+    dataKey: "lastName", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
   },
   {
-    width: 50,
-    label: "Age",
-    dataKey: "age",
-    numeric: true,
+    width: 30,
+    label: "Age", // 열의 제목으로 "Age"를 사용
+    dataKey: "age", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
+    numeric: true, // 숫자 형식으로 표시될 열인지 여부를 나타냄
   },
   {
-    width: 110,
-    label: "State",
-    dataKey: "state",
+    width: 100,
+    label: "State", // 열의 제목으로 "State"를 사용
+    dataKey: "state", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
   },
   {
-    width: 130,
-    label: "Phone Number",
-    dataKey: "phone",
+    width: 120,
+    label: "Phone Number", // 열의 제목으로 "Phone Number"를 사용
+    dataKey: "phone", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
   },
 ];
 
+// rows 배열은 테이블의 행에 대한 데이터를 담고 있음
 const rows: Data[] = Array.from({ length: 200 }, (_, index) =>
   createData(index)
 );
 
+// VirtuosoTableComponents 객체는 react-virtuoso 라이브러리에서 사용할 테이블 컴포넌트
 const VirtuosoTableComponents: TableComponents<Data> = {
   Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
     <TableContainer component={Paper} {...props} ref={ref} />
@@ -90,13 +109,14 @@ const VirtuosoTableComponents: TableComponents<Data> = {
   )),
 };
 
+// fixedHeaderContent 함수는 고정된 헤더 콘텐츠를 반환
 function fixedHeaderContent() {
   return (
     <TableRow>
       {columns.map((column) => (
         <TableCell
           key={column.dataKey}
-          variant="head"
+          variant="body"
           align={column.numeric || false ? "right" : "left"}
           style={{ width: column.width }}
           sx={{ backgroundColor: "background.paper" }}
@@ -108,6 +128,7 @@ function fixedHeaderContent() {
   );
 }
 
+// rowContent 함수는 주어진 인덱스와 데이터를 사용하여 행 콘텐츠를 반환
 function rowContent(_index: number, row: Data) {
   return (
     <React.Fragment>
@@ -123,15 +144,19 @@ function rowContent(_index: number, row: Data) {
   );
 }
 
+// ReactVirtualizedTable 컴포넌트는 react-virtuoso 라이브러리를 사용하여 가상화된 테이블을 렌더링
 export default function ReactVirtualizedTable() {
   return (
-    <Paper style={{ height: 400, width: "100%" }}>
-      <TableVirtuoso
-        data={rows}
-        components={VirtuosoTableComponents}
-        fixedHeaderContent={fixedHeaderContent}
-        itemContent={rowContent}
-      />
-    </Paper>
+    <Style>
+      <Paper style={{ height: 500, width: "100%" }}>
+        <TableVirtuoso
+          data={rows}
+          components={VirtuosoTableComponents}
+          fixedHeaderContent={fixedHeaderContent}
+          itemContent={rowContent}
+        />
+      </Paper>
+      <WriteButton></WriteButton>
+    </Style>
   );
 }
