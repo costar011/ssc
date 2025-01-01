a30d994d8471a40ca9dfab3dfb5d4504da02c681
costar011 <dnaer456@gmail.com>
2025-01-01 23:53:39 +0900

    boardlist tsx

--- src/pages/BoardList.tsx
+++ src/pages/BoardList.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Header from "../component/header";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -10,8 +11,6 @@ import { TableVirtuoso, TableComponents } from "react-virtuoso";
 import Chance from "chance";
 import styled from "@emotion/styled";
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import Header from "../component/header";
 
 interface Data {
   id: number;
@@ -34,16 +33,27 @@ const Style = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  width: 100%;
+  height: 100vh;
   position: relative;
 
-  .menu-buttons {
+  .header-line {
+    width: 100%;
+  }
+
+  .main-container {
     display: flex;
-    gap: 2em; /* 메뉴 버튼들 간격 설정 */
-    margin-left: auto; /* 메뉴 버튼을 오른쪽 정렬 */
-    margin-top: 1em;
-    margin-right: 2em;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    position: relative; /* 부모 컨테이너를 기준으로 버튼 위치 설정 */
+  }
+
+  .button_wapper {
+    display: flex;
+    justify-content: flex-end;
+    margintop: 10px;
+    gap: 0.7em;
+    margin: 10px 5px;
   }
 `;
 
@@ -68,12 +78,12 @@ const columns: ColumnData[] = [
     dataKey: "firstName", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
   },
   {
-    width: 50,
+    width: 100,
     label: "Last Name", // 열의 제목으로 "Last Name"을 사용
     dataKey: "lastName", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
   },
   {
-    width: 50,
+    width: 30,
     label: "Age", // 열의 제목으로 "Age"를 사용
     dataKey: "age", // 데이터 객체에서 해당 열의 값을 가져올 때 사용할 키
     numeric: true, // 숫자 형식으로 표시될 열인지 여부를 나타냄
@@ -91,7 +101,7 @@ const columns: ColumnData[] = [
 ];
 
 // rows 배열은 테이블의 행에 대한 데이터를 담고 있음
-const rows: Data[] = Array.from({ length: 10 }, (_, index) =>
+const rows: Data[] = Array.from({ length: 200 }, (_, index) =>
   createData(index)
 );
 
@@ -103,7 +113,7 @@ const VirtuosoTableComponents: TableComponents<Data> = {
   Table: (props) => (
     <Table
       {...props}
-      sx={{ borderCollapse: "separate", tableLayout: "auto", margin: "10px" }}
+      sx={{ borderCollapse: "separate", tableLayout: "fixed" }}
     />
   ),
   TableHead: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
@@ -115,71 +125,70 @@ const VirtuosoTableComponents: TableComponents<Data> = {
   )),
 };
 
-const SaveClick = () => {
-  alert("Save Clicked");
-};
+// fixedHeaderContent 함수는 고정된 헤더 콘텐츠를 반환
+function fixedHeaderContent() {
+  return (
+    <TableRow>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          variant="body"
+          align={column.numeric || false ? "right" : "left"}
+          style={{ width: column.width }}
+          sx={{ backgroundColor: "background.paper" }}
+        >
+          {column.label}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
 
-const BoardList = () => {
-  const navigate = useNavigate();
+// rowContent 함수는 주어진 인덱스와 데이터를 사용하여 행 콘텐츠를 반환
+function rowContent(_index: number, row: Data) {
+  return (
+    <React.Fragment>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          align={column.numeric || false ? "right" : "left"}
+        >
+          {row[column.dataKey]}
+        </TableCell>
+      ))}
+    </React.Fragment>
+  );
+}
 
-  const writeClick = () => navigate("/write");
-
-  // fixedHeaderContent 함수는 고정된 헤더 콘텐츠를 반환
-  function fixedHeaderContent() {
-    return (
-      <TableRow>
-        {columns.map((column) => (
-          <TableCell
-            key={column.dataKey}
-            variant="body"
-            align={column.numeric || false ? "right" : "left"}
-            style={{ width: column.width }}
-            sx={{ backgroundColor: "background.paper" }}
-          >
-            {column.label}
-          </TableCell>
-        ))}
-      </TableRow>
-    );
-  }
-
-  // rowContent 함수는 주어진 인덱스와 데이터를 사용하여 행 콘텐츠를 반환
-  function rowContent(_index: number, row: Data) {
-    return (
-      <React.Fragment>
-        {columns.map((column) => (
-          <TableCell
-            key={column.dataKey}
-            align={column.numeric || false ? "right" : "left"}
-          >
-            {row[column.dataKey]}
-          </TableCell>
-        ))}
-      </React.Fragment>
-    );
-  }
-
+// ReactVirtualizedTable 컴포넌트는 react-virtuoso 라이브러리를 사용하여 가상화된 테이블을 렌더링
+export default function ReactVirtualizedTable() {
   return (
     <Style>
-      <Header />
-      <Paper style={{ height: 500, width: "100%", margin: "10px" }}>
-        <TableVirtuoso
-          data={rows}
-          components={VirtuosoTableComponents}
-          fixedHeaderContent={fixedHeaderContent}
-          itemContent={rowContent}
-        />
-      </Paper>
-      <div className="menu-buttons">
-        <Button variant="outlined" onClick={writeClick}>
-          글쓰기
-        </Button>
-        <Button onClick={SaveClick} variant="outlined">
-          저장하기
-        </Button>
+      <div className="header-line">
+        <Header />
+      </div>
+      <div className="main-container">
+        <Paper
+          style={{
+            height: "90%",
+            width: "100%",
+            margin: "10px",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <TableVirtuoso
+            data={rows}
+            components={VirtuosoTableComponents}
+            fixedHeaderContent={fixedHeaderContent}
+            itemContent={rowContent}
+          />
+          <div className="button_wapper">
+            <Button variant="outlined">글쓰기</Button>
+            <Button variant="outlined">저장하기</Button>
+          </div>
+        </Paper>
       </div>
     </Style>
   );
-};
-
-export default BoardList;
+}
