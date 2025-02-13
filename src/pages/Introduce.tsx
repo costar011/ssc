4c264a68feef34b1c91be3111086a6c1839320bc
costar011 <dnaer456@gmail.com>
2025-02-13 23:30:16 +0900

    style

--- src/pages/Introduce.tsx
+++ src/pages/Introduce.tsx
@@ -3,6 +3,21 @@ import { color } from "../utils";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 
+import style from "@emotion/styled";
+
+const Style = style.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  position: relative;
+
+  .header-line {
+    width: 100%;
+  }
+`;
+
 const Introduce = () => {
     return(
         <></>
