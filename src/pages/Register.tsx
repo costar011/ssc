512c5e4dc4586e2e1f43a326a5f911e82977b269
costar011 <dnaer456@gmail.com>
2025-03-09 17:49:20 +0900

    mui ing

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,26 +6,18 @@ import styled from "@emotion/styled";
 
 import Stack from "@mui/material/Stack";
 
-const Style = styled.div` /* Style 컴포넌트 */
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  position: relative;
-
-  .header-line {
-    width: 100%;
-  }
-`;
-
 const Register = () => {
   return (
-    <Style>
-      <Stack className="header-line">
-        <Header />
-      </Stack>
-    </Style>
+    <Stack
+      direction="column"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      position="relative"
+    >
+      <Header />
+    </Stack>
   );
 };
 export default Register;
